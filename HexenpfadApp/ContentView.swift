90349a62ce03ef5498b8bf3c073fa359b8bb2199
mizech <michael.zech@hotmail.de>
2025-03-25 14:38:12 +0100

    Initial Commit

---
+++ HexenpfadApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HexenpfadApp
+//
+//  Created by Michael on 25.03.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HexenpfadApp/HexenpfadAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HexenpfadAppApp.swift
+//  HexenpfadApp
+//
+//  Created by Michael on 25.03.25.
+//
+
+import SwiftUI
+
+@main
+struct HexenpfadAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
